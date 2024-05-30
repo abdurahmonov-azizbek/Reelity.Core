ad028f9949d46c84287bebc39b47f08b90c65635
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-05-30 22:42:04 +0500

    DATA: Create VideoMetadata

---
+++ Reelity.Core.Api/Models/Metadatas/VideoMetadata.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+
+namespace Reelity.Core.Api.Models.Metadatas
+{
+    public class VideoMetadata
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string BlobPath { get; set; }
+        public string Thubnail { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
