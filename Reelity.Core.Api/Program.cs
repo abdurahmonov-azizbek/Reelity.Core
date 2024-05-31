59c2b36d6435024b499a691ed05dca03e7118475
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-05-31 23:13:40 +0500

    CODE RUB: Change Header.

--- Reelity.Core.Api/Program.cs
+++ Reelity.Core.Api/Program.cs
@@ -1,7 +1,7 @@
-// ---------------------------------------------------------------
+// -------------------------------------------------------
 // Copyright (c) Coalition of the Good-Hearted Engineers
-// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
-// ---------------------------------------------------------------
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
