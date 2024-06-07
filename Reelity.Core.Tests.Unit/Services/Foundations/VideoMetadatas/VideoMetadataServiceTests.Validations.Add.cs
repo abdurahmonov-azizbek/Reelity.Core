dd502b4dc09ced1d93ca2467450037c6577a7341
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-06-07 20:51:15 +0500

    ShouldThrowValidationExceptionOnAddIfIsNullAndLogError -> FAIL

---
+++ Reelity.Core.Tests.Unit/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Validations.Add.cs
@@ -0,0 +1,45 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using Reelity.Core.Api.Models.Metadatas;
+using Reelity.Core.Api.Models.VideoMetadatas.Exceptions;
+using System.Threading.Tasks;
+
+namespace Reelity.Core.Tests.Unit.Services.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfIsNullAndLogError()
+        {
+            //given
+            VideoMetadata nullVideoMetadata = null;
+            var nullVideoMetadataException = new NullVideoMetadataException("VideoMetadata is");
+
+            var expectedVideoMetadataValidationException =
+                new VideoMetadataValidationException(
+                    "Video Metadata Validation Exception occured, fix the errors and try again.",
+                        nullVideoMetadataException);
+
+            //when
+            ValueTask<VideoMetadata> addVideoMetadata =
+                this.videoMetadataService.AddVideoMetadataAsync(nullVideoMetadata);
+
+            VideoMetadataValidationException actualVideoMetadataValidationException =
+                await Assert.ThrowsAsync<VideoMetadataValidationException>(addVideoMetadata.AsTask);
+
+            //then
+            actualVideoMetadataValidationException.Should()
+                .BeEquivalentTo(expectedVideoMetadataValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertVideoMetadataAsync(It.IsAny<VideoMetadata>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
