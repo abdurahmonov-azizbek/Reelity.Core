e2a0725a122751979513bea5ad570edb7f280ea3
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-06-10 21:28:26 +0500

    ShouldThrowValidationExceptionOnAddIfVideoMetadataIsInvalidDataAndLogItAsync -> FAIL

--- Reelity.Core.Tests.Unit/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Validations.Add.cs
+++ Reelity.Core.Tests.Unit/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Validations.Add.cs
@@ -41,5 +41,63 @@ namespace Reelity.Core.Tests.Unit.Services.Foundations.VideoMetadatas
 
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfVideoMetadataIsInvalidDataAndLogItAsync(string invalidData)
+        {
+            //given
+            var invalidVideoMetadata = new VideoMetadata()
+            {
+                Title = invalidData
+            };
+
+            var invalidVideoMetadataException = new InvalidVideoMetadataException(
+                message: "Video Metadata is invalid.");
+
+            invalidVideoMetadataException.AddData(key: nameof(VideoMetadata.Id),
+                values: "Id is required.");
+
+            invalidVideoMetadataException.AddData(key: nameof(VideoMetadata.Title),
+                values: "Text is required.");
+
+            invalidVideoMetadataException.AddData(key: nameof(VideoMetadata.BlobPath),
+                values: "Text is required.");
+
+            invalidVideoMetadataException.AddData(key: nameof(VideoMetadata.CreatedDate),
+                values: "Date is required.");
+
+            invalidVideoMetadataException.AddData(key: nameof(VideoMetadata.UpdatedDate),
+                values: "Date is required.");
+
+            var expectedVideoMetadataValidationException =
+                new VideoMetadataValidationException(
+                    message: "Video Metadata Validation Exception occured, fix the errors and try again.",
+                    innerException: invalidVideoMetadataException);
+
+            //when
+            ValueTask<VideoMetadata> addVideoMetadataTask =
+                this.videoMetadataService.AddVideoMetadataAsync(invalidVideoMetadata);
+
+            VideoMetadataValidationException actualVideoMetadataValidationException =
+                await Assert.ThrowsAsync<VideoMetadataValidationException>(addVideoMetadataTask.AsTask);
+
+
+            //then
+            actualVideoMetadataValidationException.Should().BeEquivalentTo(expectedVideoMetadataValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedVideoMetadataValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertVideoMetadataAsync(It.IsAny<VideoMetadata>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
