649aa672e04f2f5fbe55fd564dae463a59f85f12
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-06-15 16:55:54 +0500

    ShuouldRemoveVideoMetadataByIdAsync() -> FAIL

--- Reelity.Core.Tests.Unit/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Logic.RemoveById.cs
+++ Reelity.Core.Tests.Unit/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Logic.RemoveById.cs
@@ -3,13 +3,48 @@
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
 
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore.Storage;
+using Moq;
 using Reelity.Core.Api.Models.VideoMetadatas;
+using System;
+using System.Threading.Tasks;
 
 namespace Reelity.Core.Tests.Unit.Services.Foundations.VideoMetadatas
 {
     public partial class VideoMetadataServiceTests
     {
+        [Fact]
+        public async Task ShuouldRemoveVideoMetadataByIdAsync()
+        {
+            //given
+            VideoMetadata randomVideoMetadata = CreateRandomVideoMetadata();
+            Guid videoMetadataId = randomVideoMetadata.Id;
+            VideoMetadata storageVideoMetadata = randomVideoMetadata;
+            VideoMetadata expectedVideoMetadata = storageVideoMetadata;
 
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectVideoMetadataByIdAsync(videoMetadataId))
+                .ReturnsAsync(storageVideoMetadata);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.DeleteVideoMetadataAsync(storageVideoMetadata))
+                .ReturnsAsync(expectedVideoMetadata);
+
+            //when
+            VideoMetadata actualVideoMetadata =
+                await this.videoMetadataService.RemoveVideoMetadataByIdAsync(videoMetadataId);
+
+            //then
+            actualVideoMetadata.Should().BeEquivalentTo(expectedVideoMetadata);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectVideoMetadataByIdAsync(videoMetadataId),
+                Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
