a409c43dc1310085c46ef6cbaf661e731d3e3101
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-06-15 22:47:55 +0500

    CONTROLLERS : Post VideoMetadata

---
+++ Reelity.Core.Api/Controllers/VideoMetadatasController.cs
@@ -0,0 +1,57 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using Reelity.Core.Api.Services.VideoMetadatas;
+using RESTFulSense.Controllers;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Threading.Tasks;
+using Reelity.Core.Api.Models.VideoMetadatas;
+using Reelity.Core.Api.Models.VideoMetadatas.Exceptions;
+
+namespace Reelity.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VideoMetadatasController : RESTFulController
+    {
+        private readonly IVideoMetadataService videoMetadataService;
+
+        public VideoMetadatasController(IVideoMetadataService videoMetadataService) =>
+        this.videoMetadataService = videoMetadataService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<VideoMetadata>> PostVideoMetadataAsync(VideoMetadata videoMetadata)
+        {
+            try
+            {
+                VideoMetadata postedVideoMetadata = await this.videoMetadataService.AddVideoMetadataAsync(videoMetadata);
+
+                return Created(postedVideoMetadata);
+            }
+            catch (VideoMetadataValidationException videoMetadataValidationException)
+            {
+                return BadRequest(videoMetadataValidationException.InnerException);
+            }
+            catch (VideoMetadataDependencyException videoMetadataDependencyException)
+            {
+                return InternalServerError(videoMetadataDependencyException.InnerException);
+            }
+            catch (VideoMetadataDependencyValidationException videoMetadataDependencyValidationException)
+                when (videoMetadataDependencyValidationException.InnerException is AlreadyExitsVideoMetadataException)
+            {
+                return Conflict(videoMetadataDependencyValidationException.InnerException);
+            }
+            catch (VideoMetadataDependencyValidationException videoMetadataDependencyValidationException)
+            {
+                return BadRequest(videoMetadataDependencyValidationException.InnerException);
+            }
+            catch (VideoMetadataServiceException videoMetadataServiceException)
+            {
+                return InternalServerError(videoMetadataServiceException.InnerException);
+            }
+        }
+    }
+}
--- Reelity.Core.Api/Program.cs
+++ Reelity.Core.Api/Program.cs
@@ -6,12 +6,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Reelity.Core.Api.Brokers.DateTimes;
+using Reelity.Core.Api.Brokers.Loggings;
+using Reelity.Core.Api.Brokers.Storages;
+using Reelity.Core.Api.Services.VideoMetadatas;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+AddBrokers(builder.Services);
+AddFoundationServices(builder.Services);
 
 var app = builder.Build();
 
@@ -28,3 +34,15 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void AddBrokers(IServiceCollection services)
+{
+    services.AddTransient<IStorageBroker, StorageBroker>();
+    services.AddTransient<ILoggingBroker, LoggingBroker>();
+    services.AddTransient<IDateTimeBroker, DateTimeBroker>();
+}
+
+static void AddFoundationServices(IServiceCollection services)
+{
+    services.AddTransient<IVideoMetadataService, VideoMetadataService>();
+}
