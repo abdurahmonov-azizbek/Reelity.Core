77c6f0bf38b9fcc0e4ebd8c10431914d9d787fff
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-06-28 21:20:18 +0500

    - Implement Api Broker to Post and GetAll - Add VideoMetadata Model

---
+++ Reelity.Core.Portal.Web/Brokers/API/ApiBroker.VideoMetadatas.cs
@@ -0,0 +1,22 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Reelity.Core.Portal.Web.Models.VideoMetadatas;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Reelity.Core.Portal.Web.Brokers.API
+{
+    public partial class ApiBroker
+    {
+        private const string VideoMetadatasRelativeUrl = "api/videometadatas";
+
+        public async ValueTask<VideoMetadata> PostVideoMetadataAsync(VideoMetadata videoMetadata) =>
+            await this.PostAsync(VideoMetadatasRelativeUrl, videoMetadata);
+
+        public async ValueTask<List<VideoMetadata>> GetAllVideoMetadatasAsync() =>
+            await this.GetAsync<List<VideoMetadata>>(VideoMetadatasRelativeUrl);
+    }
+}
