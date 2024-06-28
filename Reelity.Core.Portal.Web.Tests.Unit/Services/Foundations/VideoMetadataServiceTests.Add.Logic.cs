7d44c14eb91fd4fa1bc478c30f2f385f3a6b18d6
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-06-28 21:51:01 +0500

    ShouldRegisterVideoMetadataAsync -> FAIL

---
+++ Reelity.Core.Portal.Web.Tests.Unit/Services/Foundations/VideoMetadataServiceTests.Add.Logic.cs
@@ -0,0 +1,45 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using Reelity.Core.Portal.Web.Models.VideoMetadatas;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Reelity.Core.Portal.Web.Tests.Unit.Services.Foundations
+{
+    public partial class VideoMetadataServiceTests
+    {
+        [Fact]
+        public async Task ShouldRegisterVideoMetadataAsync()
+        {
+            // given
+            VideoMetadata randomVideoMetadata = CreateRandomVideoMetadata();
+            VideoMetadata inputVideoMetadata = randomVideoMetadata;
+            VideoMetadata retrievedVideoMetadata = inputVideoMetadata;
+            VideoMetadata expectedVideoMetadata = retrievedVideoMetadata;
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PostVideoMetadataAsync(inputVideoMetadata))
+                    .ReturnsAsync(retrievedVideoMetadata);
+
+            // when
+            VideoMetadata actualVideoMetadata =
+                await this.videoMetadataService
+                    .AddVideoMetadataAsync(inputVideoMetadata);
+
+            // then
+            actualVideoMetadata.Should().BeEquivalentTo(expectedVideoMetadata);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostVideoMetadataAsync(inputVideoMetadata),
+                    Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Reelity.Core.Portal.Web.Tests.Unit/Services/Foundations/VideoMetadataServiceTests.cs
@@ -0,0 +1,65 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Moq;
+using Reelity.Core.Portal.Web.Brokers.API;
+using Reelity.Core.Portal.Web.Brokers.Loggings;
+using Reelity.Core.Portal.Web.Services.Foundations.VideoMetadatas;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System;
+using Tynamix.ObjectFiller;
+using Xeptions;
+using Reelity.Core.Portal.Web.Models.VideoMetadatas;
+using System.Linq;
+
+namespace Reelity.Core.Portal.Web.Tests.Unit.Services.Foundations
+{
+    public partial class VideoMetadataServiceTests
+    {
+        private readonly Mock<IApiBroker> apiBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IVideoMetadataService videoMetadataService;
+
+        public VideoMetadataServiceTests()
+        {
+            this.apiBrokerMock = new Mock<IApiBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.videoMetadataService = new VideoMetadataService(
+                apiBroker: this.apiBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static VideoMetadata CreateRandomVideoMetadata() =>
+            CreateVideoMetadataFiller().Create();
+
+        private static List<VideoMetadata> CreateRandomVideoMetadatas() =>
+            CreateVideoMetadataFiller().Create(count: GetRandomNumber()).ToList();
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException)
+        {
+            return actualException => actualException.Message == expectedException.Message
+                && actualException.InnerException.Message == expectedException.InnerException.Message
+                && (actualException.InnerException as Xeption).DataEquals(expectedException.InnerException.Data);
+        }
+
+        private static string GetRandomString() => 
+            new MnemonicString().GetValue();
+
+        private static int GetRandomNumber() => 
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static Filler<VideoMetadata> CreateVideoMetadataFiller()
+        {
+            var filler = new Filler<VideoMetadata>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(DateTimeOffset.UtcNow);
+
+            return filler;
+        }
+    }
+}
