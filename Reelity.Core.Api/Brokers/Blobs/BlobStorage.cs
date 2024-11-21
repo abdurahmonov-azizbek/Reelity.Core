18c815ecacd905611911dfb39f497fe749c46b09
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-11-21 16:10:46 +0500

    CODE RUB: Update BlobBroker

--- Reelity.Core.Api/Brokers/Blobs/BlobStorage.cs
+++ Reelity.Core.Api/Brokers/Blobs/BlobStorage.cs
@@ -3,12 +3,10 @@
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
 
-using Azure;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Reelity.Core.Api.Models.Blobs;
 using System.Collections.Generic;
 using System.IO;
@@ -16,17 +14,15 @@ using System.Threading.Tasks;
 
 namespace Reelity.Core.Api.Brokers.Blobs
 {
-    public class BlobStorage 
+    public class BlobStorage
     {
         private readonly string storageConnectionString;
         private readonly string storageContainerName;
-        private readonly ILogger<BlobStorage> logger;
 
-        public BlobStorage(IConfiguration configuration, ILogger<BlobStorage> logger)
+        public BlobStorage(IConfiguration configuration)
         {
             this.storageConnectionString = configuration.GetValue<string>("BlobConnectionString");
             this.storageContainerName = configuration.GetValue<string>("BlobContainerName");
-            this.logger = logger;
         }
 
         public async Task<BlobResponse> DeleteAsync(string blobFilename)
@@ -34,17 +30,7 @@ namespace Reelity.Core.Api.Brokers.Blobs
             BlobContainerClient client = new BlobContainerClient(storageConnectionString, storageContainerName);
 
             BlobClient file = client.GetBlobClient(blobFilename);
-
-            try
-            {
-                await file.DeleteAsync();
-            }
-            catch (RequestFailedException ex)
-                when (ex.ErrorCode == BlobErrorCode.BlobNotFound)
-            {
-                logger.LogError($"File {blobFilename} was not found.");
-                return new BlobResponse { Error = true, Status = $"File with name {blobFilename} not found." };
-            }
+            await file.DeleteAsync();
 
             return new BlobResponse { Error = false, Status = $"File: {blobFilename} has been successfully deleted." };
         }
@@ -52,26 +38,17 @@ namespace Reelity.Core.Api.Brokers.Blobs
         public async Task<Blob> DownloadAsync(string blobFilename)
         {
             BlobContainerClient client = new BlobContainerClient(storageConnectionString, storageContainerName);
+            BlobClient file = client.GetBlobClient(blobFilename);
 
-            try
+            if (await file.ExistsAsync())
             {
-                BlobClient file = client.GetBlobClient(blobFilename);
+                var data = await file.OpenReadAsync();
+                Stream blobContent = data;
+                var content = await file.DownloadContentAsync();
+                string name = blobFilename;
+                string contentType = content.Value.Details.ContentType;
 
-                if (await file.ExistsAsync())
-                {
-                    var data = await file.OpenReadAsync();
-                    Stream blobContent = data;
-                    var content = await file.DownloadContentAsync();
-                    string name = blobFilename;
-                    string contentType = content.Value.Details.ContentType;
-
-                    return new Blob { Content = blobContent, Name = name, ContentType = contentType };
-                }
-            }
-            catch (RequestFailedException ex)
-                when (ex.ErrorCode == BlobErrorCode.BlobNotFound)
-            {
-                logger.LogError($"File {blobFilename} was not found.");
+                return new Blob { Content = blobContent, Name = name, ContentType = contentType };
             }
 
             return null;
@@ -105,37 +82,17 @@ namespace Reelity.Core.Api.Brokers.Blobs
             BlobResponse response = new();
             BlobContainerClient container = new BlobContainerClient(storageConnectionString, storageContainerName);
             await container.CreateIfNotExistsAsync();
+            BlobClient client = container.GetBlobClient(blob.FileName);
 
-            try
+            await using (Stream data = blob.OpenReadStream())
             {
-                BlobClient client = container.GetBlobClient(blob.FileName);
-
-                await using (Stream data = blob.OpenReadStream())
-                {
-                    await client.UploadAsync(data);
-                }
-
-                response.Status = $"File {blob.FileName} Uploaded Successfully";
-                response.Error = false;
-                response.Blob.Uri = client.Uri.AbsoluteUri;
-                response.Blob.Name = client.Name;
-
+                await client.UploadAsync(data);
             }
-            catch (RequestFailedException ex)
-                when (ex.ErrorCode == BlobErrorCode.BlobAlreadyExists)
-            {
-                logger.LogError($"File with name {blob.FileName} already exists in container. Set another name to store the file in the container: '{storageContainerName}.'");
-                response.Status = $"File with name {blob.FileName} already exists. Please use another name to store your file.";
-                response.Error = true;
-                return response;
-            }
-            catch (RequestFailedException ex)
-            {
-                logger.LogError($"Unhandled Exception. ID: {ex.StackTrace} - Message: {ex.Message}");
-                response.Status = $"Unexpected error: {ex.StackTrace}. Check log with StackTrace ID.";
-                response.Error = true;
-                return response;
-            }
+
+            response.Status = $"File {blob.FileName} Uploaded Successfully";
+            response.Error = false;
+            response.Blob.Uri = client.Uri.AbsoluteUri;
+            response.Blob.Name = client.Name;
 
             return response;
         }
